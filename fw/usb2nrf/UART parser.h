817220058fda9df02b07fcf42e2e12548d8ef5c3
Mintytail <jskrat@gmail.com>
2020-04-20 20:25:07 +0300

    fw: complete moving packet parsing functional to UART protocol.c remove extra includes

--- fw/usb2nrf/UART parser.h
+++ fw/usb2nrf/UART parser.h
@@ -8,30 +8,6 @@
 
 #ifndef UART_PARSER_H_
 #define UART_PARSER_H_
-#include "defines.h"
-#include <stdint.h>
-#ifndef UNIT_TESTING
-    #include <avr/pgmspace.h>
-#endif
-#include "messages.h"
-#include "avr-nrf24l01-master/src/nrf24l01-mnemonics.h"
-#include "avr-nrf24l01-master/src/nrf24l01.h"
-#include "../usb2nrf/UART functions.h"
-#include "../usb2nrf/UART protocol.h"
-
-// packet prefixes
-#define uPPProtoVer 0
-
-typedef enum {
-	usError,
-	usProtoVer,
-	usCommand,
-	usPDataLength,
-	usPData,
-	usEnd,
-} usState;
-
-//union uPackage packageBuffer;
 
 //uint8_t sendBufferBegin;
 //uint8_t sendBufferEnd;
@@ -39,11 +15,6 @@ typedef enum {
 //uint16_t badRFPackets;
 
 void u_init();
-void parse(unsigned char b);
-#define U_TRANSMIT_START UCSR0B |= (1 << UDRIE0)
-void uQueueChar(const uint8_t c);
-void uQueueString(const string *data);
-void uSendPacket(uPackage *packet);
 
 
 #endif /* UART_PARSER_H_ */
--- fw/usb2nrf/UART protocol.c
+++ fw/usb2nrf/UART protocol.c
@@ -9,10 +9,35 @@
 #include "UART functions.h"
 #include "string.h"
 #include <stddef.h>
+#include <stdbool.h>
+#include "messages.h"
+#include "ui.h"
 
-void UARTGenerateResponse(const uPackage const *request, uPackage *response) {
+// packet prefixes
+#define uPPProtoVer 0
+
+typedef enum {
+	usError,
+	usProtoVer,
+	usCommand,
+	usPDataLength,
+	usPData,
+	usEnd,
+} usState;
+
+static usState state;
+static uint8_t position = 0;
+static uPackage reqBuffer;
+
+
+void UARTGenerateResponse(const uPackage *request, uPackage *response) {
+	response->pkg.protocolVersion = uPPProtoVer;
 	response->pkg.command = request->pkg.command | 0x80;
 	response->pkg.payloadSize = 1;
+	if (uPPProtoVer != request->pkg.protocolVersion) {
+		response->pkg.payload[0] = eucBadVersion;
+		return;
+	}
 	// search for the requested command
 	for (uint8_t i = 0; i < UART_FUNCTIONS_NUMBER; i++) {
 		eModemCommand commandToCheck = pgm_read_byte(UARTFunctions[i].command);
@@ -32,4 +57,77 @@ void UARTGenerateResponse(const uPackage const *request, uPackage *response) {
 	}
 	// command not found, generate error response about it
 	response->pkg.payload[0] = eucBadCommand;
+}
+
+void UARTBeginTransaction() {
+	state = usProtoVer;
+	ui_subsystem_str(ui_s_uart_packet_fsm, &m_usProtoVer, true);
+}
+
+bool UARTProcessNextByte(uint8_t b, uPackage *response) {
+	switch (state) {
+		case usEnd:
+		case usError: {
+			//ui_subsystem_str(ui_s_uart_packet_fsm, &m_usError, true);
+			break;
+		}
+		case usProtoVer: {
+			if (uPPProtoVer == b) {
+				state = usCommand;
+				ui_subsystem_str(ui_s_uart_packet_fsm, &m_usCommand, true);
+				} else {
+				state = usError;
+				ui_subsystem_str(ui_s_uart_packet_fsm, &m_usErrorProto, true);
+			}
+			break;
+		}
+		case usCommand: {
+			if (0x80 & b) {
+				state = usError;
+				ui_subsystem_str(ui_s_uart_packet_fsm, &m_usErrorCmd, true);
+				} else {
+				reqBuffer.pkg.command = b;
+				state = usPDataLength;
+				ui_subsystem_str(ui_s_uart_packet_fsm, &m_usPDataLength, true);
+			}
+			break;
+		}
+		case usPDataLength: {
+			if (PAYLOAD_SIZE + MAC_SIZE <= b) {
+				state = usError;
+				ui_subsystem_str(ui_s_uart_packet_fsm, &m_usErrorLength, true);
+				break;
+			}
+			reqBuffer.pkg.payloadSize = b;
+			position = 0;
+			if (0 == b) {
+				// there won't be any bytes from that package anymore
+				state = usError;
+				ui_subsystem_str(ui_s_uart_packet_fsm, &m_usEnd, true);
+				UARTGenerateResponse(&reqBuffer, response);
+				return true;
+			} else {
+				state = usPData;
+				ui_subsystem_str(ui_s_uart_packet_fsm, &m_usPData, true);
+			}
+			break;
+		}
+		case usPData: {
+			reqBuffer.pkg.payload[position++] = b;
+			if (reqBuffer.pkg.payloadSize <= position) {
+				// that's all payload, no bytes from that package left
+				state = usError;
+				ui_subsystem_str(ui_s_uart_packet_fsm, &m_usEnd, true);
+				UARTGenerateResponse(&reqBuffer, response);
+				return true;
+			}
+			break;
+		}
+	}
+	return false;
+}
+
+void UARTProtocolInit() {
+	state = usError;
+	ui_subsystem_str(ui_s_uart_packet_fsm, &m_usNotInit, true);
 }
